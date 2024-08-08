e61cb1ae10efda550fac6f0027da168f67f7562e
ciwei04 <13055285256@163.com>
2024-08-08 09:27:03 +0800

    feat: 测试合并目录

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-// env.d.ts
-declare module "*.vue" {
-    import { DefineComponent } from "vue"
-    const component: DefineComponent<{}, {}, any>
-    export default component
-}
-declare interface Window {
-    Vue: any,
-}
